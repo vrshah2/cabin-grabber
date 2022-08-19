e8ea011ba2db7c747c38fccdf0611ebc21e9ab27
Trygve Karper <trygve@databutton.io>
2022-08-19 11:02:08 +0200

    fixed up dates ++

--- apps/main.py
+++ apps/main.py
@@ -4,26 +4,12 @@ import databutton as db
 import numpy as np
 import pandas as pd
 import requests as rq
-from .tools import list_of_dates_to_date_ranges
+from .tools import get_availability
 import datetime
 from email_validator import validate_email
 
 DATA_KEY = 'user-subscriptions'
 
-#month_lookup = {
-    #'January':1,
-    #'February':2,
-    #'March':3,
-    #'April':4,
-    #'May':5,
-    #'June':6,
-    #'July':7,
-    #'August':8,
-    #'September':9,
-    #'October':10,
-    #'November':11,
-    #'December':12
-#}
 
 @db.apps.streamlit('/apps/cabin-grabber',name="Cabin Grabber")
 def cabin_grabber():
@@ -34,22 +20,21 @@ def cabin_grabber():
     cabin_selection = st.text_input(label="Enter the cabin's Visbook URL (e.g. https://reservations.visbook.com/5471)")
     st.caption('Note: To get the Visbook URL for a cabin, find the cabin on UT.no and click Bestill overnatting')
     st.subheader('What dates are you interested in monitoring?')
-    startDate = st.date_input(
-        'From',
-    )
-    endDate = st.date_input(
-        'To',
-    )
+
+    startDate = st.date_input('From', min_value=datetime.datetime.today())
+    endDate = st.date_input('To', min_value=datetime.datetime.today())
     #st.subheader('Which months are you looking for availability in, good sir/mlady? 🎩')
     #months = st.multiselect('Select the month(s)', ['January','February','March','April','May','June','July','August','September','October','November','December'])
     st.caption('Note: You will get an email whenever your cabin has avaibility in the range you selected')
     st.subheader('Last step, can I haz your email?')
+
     email_input = st.text_input(label='Email address')
     email = None
     try:
         email = validate_email(email_input).email
     except:
         pass
+
     if st.button('Submit'):
         if email is not None:
             df = db.storage.dataframes.get(DATA_KEY)
@@ -68,49 +53,18 @@ def cabin_grabber():
         else:
             st.write("Your email is invalid. Sort it out bub.")
 
+
+
+
 @db.jobs.repeat_every(seconds=60*60*24)
 def check_availability():
     df = db.storage.dataframes.get(DATA_KEY)
     for index, row in df.iterrows():
-        months = [month_lookup[month] for month in row.months]
+        start_date = row['startDate']
+        end_date   = row['endDate']
         locationId = row['cabin_url'].split('/')[-1]
-        response = rq.get(f"https://ws.visbook.com/8/api/{locationId}/webproducts")
-        info = response.json()
-        if 'error' in info:
-            continue
-        hytter = []
-        for hytte in info:
-            hytter.append({
-                'id': hytte['webProductId'],
-                'name': hytte['unitName']
-            })
-
-        summary = []
-        for hytte in hytter:
-            for month in months:
-                url = f"https://ws.visbook.com/8/api/{locationId}/availability/{hytte['id']}/2022-{month}"
-                response = rq.get(url)
-                tories = response.json()
-                if 'error' in tories:
-                    continue
-                items_list = tories['items']
-                available_days = []
-                for item in items_list:
-                    date = item['date']
-                    webProducts = item['webProducts']
-                    if len(webProducts) > 0:
-                        product = webProducts[0]
-                        if 'availability' in product:
-                            is_available = product['availability']['available']
-                            if is_available:
-                                available_days.append(date)
-            
-            available_ranges = list_of_dates_to_date_ranges(available_days)
-            summary.append({
-                "name": hytte["name"],
-                "id": hytte["id"],
-                "available_days": available_ranges,
-            })
+        
+        summary = get_availability(locationId, start_date, end_date)
         df = pd.DataFrame(summary)
         html = df.to_html()
         db.notify.email(
--- apps/tools.py
+++ apps/tools.py
@@ -1,4 +1,10 @@
 import pandas as pd
+import requests as rq
+import databutton as db
+from datetime import datetime
+#
+#  *** CONVERT LIST OF DATES TO LIST OF DATE RANGES ***
+#
 
 def get_range_string(t_from, t_to):
     str_from = str(t_from.day) + '.'+ str(t_from.month)
@@ -20,3 +26,83 @@ def list_of_dates_to_date_ranges(data):
     ranges.append(get_range_string(dg[fra], dg[to+1]))
     
     return ranges
+
+
+def filter_list_of_dates(datelist, start_date, end_date):
+    dg = pd.to_datetime(datelist)
+    start = pd.to_datetime(start_date)
+    end   = pd.to_datetime(end_date)
+
+    return dg[((dg >= start) & (dg <= end))]
+
+
+
+
+def get_months(start, end):
+    print(start)
+    month_start = int(pd.to_datetime(start).month)
+    month_end   = int(pd.to_datetime(end).month)
+
+    return list(range(month_start, month_end+1))
+    
+
+
+#
+#  *** Ask for availability of a cabin
+#
+def get_list_of_cabin_options(locationId):
+    response = rq.get(f"https://ws.visbook.com/8/api/{locationId}/webproducts")
+    info = response.json()
+    if 'error' in info:
+        return -1
+
+    
+    hytter = []
+    for hytte in info:
+        hytter.append({
+            'id': hytte['webProductId'],
+            'name': hytte['unitName']
+        })
+
+    return hytter
+
+
+
+
+
+
+def get_availability(locationId, start_date, end_date):
+    hytter = get_list_of_cabin_options(locationId)
+    months = get_months(start_date, end_date)
+
+    summary = []
+    for hytte in hytter:
+            available_days = []
+            for month in months:
+                url = f"https://ws.visbook.com/8/api/{locationId}/availability/{hytte['id']}/2022-{month}"
+                response = rq.get(url)
+                tories = response.json()
+                if 'error' in tories:
+                    continue
+                items_list = tories['items']
+                for item in items_list:
+                    date = item['date']
+                    webProducts = item['webProducts']
+                    if len(webProducts) > 0:
+                        product = webProducts[0]
+                        if 'availability' in product:
+                            is_available = product['availability']['available']
+                            if is_available:
+                                available_days.append(date)
+
+            available_days = filter_list_of_dates(available_days, start_date, end_date)
+            available_ranges = list_of_dates_to_date_ranges(available_days)
+            summary.append({
+                "name": hytte["name"],
+                "id": hytte["id"],
+                "available_days": available_ranges
+            })
+
+    return summary 
+
+
